1c7ec6fafc138117dd719edeef77d378acbcfaba
Marcelo <ma.novoac@duocuc.cl>
2024-06-05 10:56:17 -0400

    Primera version

---
+++ ejemplo_funciones.py
@@ -0,0 +1,23 @@
+def sumar_2_numeros():
+    num1 = float(input("ingrese primer numero: "))
+    num2 = float(input("ingrese segundo numero: "))
+    total = num1+num2
+    print("Funcion: el total de la suma es: ", total)
+
+print("""MENU      
+1. sumar 2 numeros
+2. restar 2 numeros
+3. multiplicar 2 numeros
+4. dividir 2 numeros""")
+
+opc =int(input("ingrese opcion: "))
+if opc==1:
+    sumar_2_numeros()
+elif opc==2:
+    pass
+
+elif opc==3:
+    pass
+
+elif opc==4:
+    pass
